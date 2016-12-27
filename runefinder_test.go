9e69b229accaa75f0f2941aa990de5d130855989
Luciano Ramalho <luciano@ramalho.org>
2016-12-27 06:58:58 -0200

    passo 03: a função listar e os testes-exemplo

--- runefinder_test.go
+++ runefinder_test.go
@@ -1,16 +1,43 @@
 package runefinder
 
-import "testing"
+import (
+	"strings"
+	"testing"
+)
 
 const linhaLetraA = "0041;LATIN CAPITAL LETTER A;Lu;0;L;;;;;N;;;;0061;"
+
+const linhas3Da43 = `
+003D;EQUALS SIGN;Sm;0;ON;;;;;N;;;;;
+003E;GREATER-THAN SIGN;Sm;0;ON;;;;;Y;;;;;
+003F;QUESTION MARK;Po;0;ON;;;;;N;;;;;
+0040;COMMERCIAL AT;Po;0;ON;;;;;N;;;;;
+0041;LATIN CAPITAL LETTER A;Lu;0;L;;;;;N;;;;0061;
+0042;LATIN CAPITAL LETTER B;Lu;0;L;;;;;N;;;;0062;
+0043;LATIN CAPITAL LETTER C;Lu;0;L;;;;;N;;;;0063;
+`
 
 func TestAnalisarLinha(t *testing.T) {
 	runa, nome := AnalisarLinha(linhaLetraA)
 	if runa != 'A' {
-		t.Errorf("Esperava 'A', veio %q", runa) // <1>
+		t.Errorf("Esperava 'A', veio %q", runa)
 	}
-	const nomeA = "LATIN CAPITAL LETTER A" // <2>
+	const nomeA = "LATIN CAPITAL LETTER A"
 	if nome != nomeA {
-		t.Errorf("Esperava %q, veio %q", nomeA, nome) // <3>
+		t.Errorf("Esperava %q, veio %q", nomeA, nome)
 	}
+}
+
+func ExampleListar() {
+	texto := strings.NewReader(linhas3Da43)
+	Listar(texto, "MARK")
+	// Output: U+003F	?	QUESTION MARK
+}
+
+func ExampleListar_doisResultados() { // <1>
+	texto := strings.NewReader(linhas3Da43)
+	Listar(texto, "SIGN") // <2>
+	// Output:
+	// U+003D	=	EQUALS SIGN
+	// U+003E	>	GREATER-THAN SIGN
 }
--- ucdlib.go
+++ ucdlib.go
@@ -1,12 +1,32 @@
 package runefinder
 
-import ( // <1>
+import (
+	"bufio"
+	"fmt"
+	"io"
 	"strconv"
 	"strings"
 )
 
-func AnalisarLinha(ucdLine string) (rune, string) {
-	campos := strings.Split(ucdLine, ";")            // <2>
-	código, _ := strconv.ParseInt(campos[0], 16, 32) // <3>
-	return rune(código), campos[1]                   // <4>
+// AnalisarLinha devolve a runa e o nome de uma linha do UnicodeData.txt
+func AnalisarLinha(linha string) (rune, string) {
+	campos := strings.Split(linha, ";")
+	código, _ := strconv.ParseInt(campos[0], 16, 32)
+	return rune(código), campos[1]
+}
+
+// Listar exibe na saída padrão o código, a runa e o nome dos caracteres Unicode
+// cujo nome contem o texto da consulta // <1>
+func Listar(texto io.Reader, consulta string) {
+	varredor := bufio.NewScanner(texto) // <2>
+	for varredor.Scan() {               // <3>
+		linha := varredor.Text()            // <4>
+		if strings.TrimSpace(linha) == "" { // <5>
+			continue
+		}
+		runa, nome := AnalisarLinha(linha)    // <6>
+		if strings.Contains(nome, consulta) { // <7>
+			fmt.Printf("U+%04X\t%[1]c\t%s\n", runa, nome) // <8>
+		}
+	}
 }
