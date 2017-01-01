a816fc239745fd4d98fd83565da1f585142a8bba
Luciano Ramalho <luciano@ramalho.org>
2017-01-01 18:18:07 -0200

    ajustes no passo-06

---
+++ respostas/06/runefinder.go
@@ -0,0 +1,81 @@
+package main
+
+import (
+	"bufio"
+	"fmt"
+	"io"
+	"os"
+	"strconv"
+	"strings"
+)
+
+// AnalisarLinha devolve a runa, o nome e uma fatia de palavras que
+// ocorrem no campo nome de uma linha do UnicodeData.txt
+func AnalisarLinha(linha string) (rune, string, []string) {
+	campos := strings.Split(linha, ";")
+	código, _ := strconv.ParseInt(campos[0], 16, 32)
+	nome := campos[1]
+	palavras := separar(campos[1])
+	if campos[10] != "" { // ➊
+		nome += fmt.Sprintf(" (%s)", campos[10])
+		for _, palavra := range separar(campos[10]) { // ➋
+			if !contém(palavras, palavra) { // ➌
+				palavras = append(palavras, palavra) // ➍
+			}
+		}
+	}
+	return rune(código), nome, palavras
+}
+
+func contém(fatia []string, procurado string) bool {
+	for _, item := range fatia {
+		if item == procurado {
+			return true // ➋
+		}
+	}
+	return false // ➌
+}
+
+func contémTodos(fatia []string, procurados []string) bool {
+	for _, procurado := range procurados {
+		if !contém(fatia, procurado) {
+			return false
+		}
+	}
+	return true
+}
+
+func separar(s string) []string { // ➊
+	separador := func(c rune) bool { // ➋
+		return c == ' ' || c == '-'
+	}
+	return strings.FieldsFunc(s, separador) // ➌
+}
+
+// Listar exibe na saída padrão o código, a runa e o nome dos caracteres Unicode
+// cujo nome contem as palavras da consulta.
+func Listar(texto io.Reader, consulta string) {
+	termos := separar(consulta)
+	varredor := bufio.NewScanner(texto)
+	for varredor.Scan() {
+		linha := varredor.Text()
+		if strings.TrimSpace(linha) == "" {
+			continue
+		}
+		runa, nome, palavrasNome := AnalisarLinha(linha) // ➊
+		if contémTodos(palavrasNome, termos) {           // ➋
+			fmt.Printf("U+%04X\t%[1]c\t%s\n", runa, nome)
+		}
+	}
+}
+
+func main() {
+	ucd, err := os.Open("UnicodeData.txt")
+	if err != nil {
+		fmt.Println(err.Error())
+		os.Exit(1)
+	}
+	defer func() { ucd.Close() }()
+	consulta := strings.Join(os.Args[1:], " ")
+	Listar(ucd, strings.ToUpper(consulta))
+}
